1230517873b4db89fd48e44c0fd8969c759384a0
Jeff Kranking <jkranking@gmail.com>
2017-07-13 10:03:37 -0700

    adds crud endpoints

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController'); // returns a function that sets up the seed data input for the express app
+var apiController = require('./controllers/apiController');
+
 
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+// building out api endpoints
+
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
 
