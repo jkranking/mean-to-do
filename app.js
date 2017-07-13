f75b15e5741808128a7cc8e3072a0d09b210dee8
Jeff Kranking <jkranking@gmail.com>
2017-07-13 09:10:15 -0700

    adds seed data via an api input

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
+var setupController = require('./controllers/setupController'); // returns a function that sets up the seed data input for the express app
 
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(_dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
+setupController(app);
 
 app.listen(port);
 
+// nodemon app.js to start server
--- models/todoModel.js
+++ models/todoModel.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var todoSchema = new Schema({
-	username: String,
-	todo: String,
-	isDone: Boolean,
-	hasAttachment: Boolean
+    username: String,
+    todo: String,
+    isDone: Boolean,
+    hasAttachment: Boolean
 });
 
 var Todos = mongoose.model('Todos', todoSchema);
